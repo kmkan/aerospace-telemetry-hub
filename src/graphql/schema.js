0517eb97931081ec7e1b6037339c231a7bce837b
kmkan <kandpalkbusiness@gmail.com>
2025-04-29 12:49:15 -0600

    Added basic GraphQL schema to query telemetry data

--- src/graphql/schema.js
+++ src/graphql/schema.js
@@ -1,8 +1,28 @@
-const { GraphQLSchema, GraphQLObjectType } = require('graphql');
+const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLFloat, GraphQLList, GraphQLInt } = require('graphql');
+const pool = require('../config/db');
+
+const TelemetryType = new GraphQLObjectType({
+  name: 'Telemetry',
+  fields: {
+    id: { type: GraphQLInt },
+    aircraft_id: { type: GraphQLString },
+    metric_type: { type: GraphQLString },
+    value: { type: GraphQLFloat },
+    timestamp: { type: GraphQLString },
+  }
+});
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
-  fields: {}
+  fields: {
+    telemetry: {
+      type: new GraphQLList(TelemetryType),
+      resolve: async () => {
+        const res = await pool.query('SELECT * FROM telemetry ORDER BY timestamp DESC LIMIT 10');
+        return res.rows;
+      }
+    }
+  }
 });
 
 module.exports = new GraphQLSchema({
--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
+const app = require('./app');
 const pool = require('./config/db');
 const generateTelemetry = require('./simulator/simulator');
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 pool.query('SELECT NOW()')
   .then(res => console.log('DB Connected:', res.rows[0]))
   .catch(err => console.error('DB Error', err));
 
-setInterval(() => {
-  generateTelemetry();
-}, 3000);
+// setInterval(() => {
+//   generateTelemetry();
+// }, 3000);
